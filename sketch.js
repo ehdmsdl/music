4365fc0de328654524ebc2803adb47556a5f44bc
ehdmsdl <119025542+ehdmsdl@users.noreply.github.com>
2022-11-25 13:41:20 +0900

    1-2
    도형추가

--- sketch.js
+++ sketch.js
@@ -4,3 +4,12 @@ function setup(){
     mySound = loadSound('assets/doorbell');
     mySound.play();
 }
+
+function loadmusic(){
+    mySound.play();
+}
+
+function draw(){
+    Fill(255,0,0);
+    ellipse(50,50,100,100);
+}
