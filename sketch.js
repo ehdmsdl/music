547dc00253ab4912bd6a84de8b24c2b0934ca0f0
ehdmsdl <119025542+ehdmsdl@users.noreply.github.com>
2022-11-25 14:28:32 +0900

    최수종

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
-// let mySound;
-// function setup(){
-//     soundForamats('mp3','ogg');
-//     mySound = loadSound('assets/doorbell');
-//     mySound.play();
-// }
+let mySound;
+let botton;
+
+function setup(){
+    createCanvas(640,480);
+    soundFormats('mp3','ogg');
+    mySound = loadSound('cpr',loadMusic);
+    //mySound.play();
+    button=createButton('play');
+    button.mousePressed(togglePlaying);
+}
 
 // function loadmusic(){
 //     mySound.play();
@@ -14,27 +19,32 @@
 //     ellipse(50,50,100,100);
 // }
 
-var song;
-var button;
+// var song;
+// var button;
 
-function setup(){
-    createCanvas(200,200);
-    song=loadSound('cpr.mp3',loaded);
-    button=createButton('play');
-    button.mousePressed(togglePlaying);
-    background(51);
-}
+// function setup(){
+//     createCanvas(200,200);
+//     song=loadSound('cpr.mp3',loaded);
+//     button=createButton('play');
+//     button.mousePressed(togglePlaying);
+//     background(51);
+// }
 
-function togglePlay(){
-    if(!song.isPlaying()){
-        song.setVolume(0.3);
+function togglePlaying(){
+    if(!mySound.isPlaying()){
+        mySound.play();
         button.html('pause');
     }else{
-        song.stop();
+        mySound.stop();
         button.html('play');
     }
 }
 
-function loaded(){
-    console.log('loaded');
+function loadMusic(){
+    console.log("It's Working");
+}
+
+function draw(){
+    fill(255,0,0);
+    ellipse(50,50,100,100);
 }
