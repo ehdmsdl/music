02bf3b3233ec8287cb0489fe17844bb8306e46b2
ehdmsdl <119025542+ehdmsdl@users.noreply.github.com>
2022-11-25 13:58:07 +0900

    play button
    추가

--- sketch.js
+++ sketch.js
@@ -1,15 +1,40 @@
-let mySound;
+// let mySound;
+// function setup(){
+//     soundForamats('mp3','ogg');
+//     mySound = loadSound('assets/doorbell');
+//     mySound.play();
+// }
+
+// function loadmusic(){
+//     mySound.play();
+// }
+
+// function draw(){
+//     Fill(255,0,0);
+//     ellipse(50,50,100,100);
+// }
+
+var song;
+var button;
+
 function setup(){
-    soundForamats('mp3','ogg');
-    mySound = loadSound('assets/doorbell');
-    mySound.play();
+    createCanvas(200,200);
+    song=loadSound('cpr.mp3',loaded);
+    button=createButton('play');
+    button.mousePressed(togglePlaying);
+    background(51);
 }
 
-function loadmusic(){
-    mySound.play();
+function togglePlay(){
+    if(!song.isPlaying()){
+        song.setVolume(0.3);
+        button.html('pause');
+    }else{
+        song.stop();
+        button.html('play');
+    }
 }
 
-function draw(){
-    Fill(255,0,0);
-    ellipse(50,50,100,100);
+function loaded(){
+    console.log('loaded');
 }
