eb88614e115ca35ece67675307b3d76f7bf402fd
ehdmsdl <119025542+ehdmsdl@users.noreply.github.com>
2022-12-09 14:40:36 +0900

    시작,멈춤,점프진행중

--- sketch.js
+++ sketch.js
@@ -1,50 +1,126 @@
-let mySound;
-let botton;
+let musicFile;
+let musicFile2;
+let button;  
+let button2;
+let button3;
+let button4
+let img, song;
+let vol = 0.5;
+var jumpButton;
+var t=0;
+let amp;
+var rB;
+let music;
 
-function setup(){
-    createCanvas(640,480);
-    soundFormats('mp3','ogg');
-    mySound = loadSound('cpr',loadMusic);
-    //mySound.play();
-    button=createButton('play');
-    button.mousePressed(togglePlaying);
+function setup()
+{
+    createCanvas(640, 480);
+    background(0);
+    soundFormats('mp3', 'ogg');
+    musicFile = loadSound('cpr', loadMusic);
+    musicFile2 = loadSound('64', loadMusic);
+
+    button = createButton("play");
+    button.mousePressed(togglePlaying1);
+
+    button2 = createButton("Music1");
+    button2.mousePressed(togglePlaying2);
+    
+    music = 1;
+
+    button3 = createButton("jump");
+    button3.mousePressed(togglePlaying3);
+
+    amp = new p5.Amplitude();
 }
-
-// function loadmusic(){
-//     mySound.play();
-// }
-
-// function draw(){
-//     Fill(255,0,0);
-//     ellipse(50,50,100,100);
-// }
-
-// var song;
-// var button;
-
-// function setup(){
-//     createCanvas(200,200);
-//     song=loadSound('cpr.mp3',loaded);
-//     button=createButton('play');
-//     button.mousePressed(togglePlaying);
-//     background(51);
-// }
-
-function togglePlaying(){
-    if(!mySound.isPlaying()){
-        mySound.play();
-        button.html('pause');
-    }else{
-        mySound.stop();
-        button.html('play');
+function togglePlaying1(){
+    if(music === 1){
+        if(!musicFile.isPlaying()){
+            // rB=musicFile.reverseBuffer();
+            musicFile.setVolume(0.5);
+            musicFile.rate(1);
+            musicFile.play();
+            button.html('pause');
+        }else{
+            musicFile.stop();
+            button.html('play');
+        }
     }
-}
+    if(music === 2){
+        if(!musicFile2.isPlaying()){
+            // rB=musicFile.reverseBuffer();
+            // musicFile2.jump(200);
+            musicFile2.setVolume(0.5);
+            musicFile.rate(1);
+            musicFile2.play();
+            button.html('pause');
+        }else{
+            musicFile2.stop();
+            button.html('play');
+        }
+    }
+ }
 
-function loadMusic(){
+ function togglePlaying2(){
+    if(music === 1){
+        music = 2;
+        button2.html('Music2');
+    }else{
+        music = 1;
+        button2.html('Music1');
+    }
+    console.log(music);
+ }
+
+ function togglePlaying3(){
+    musicFile.duration();
+
+ }
+
+function loadMusic()
+{
     console.log("It's Working");
 }
 
 function draw(){
-    fill(255,0,0);
-    ellipse(50,50,100,100);
+console.log(amp.getLevel(), musicFile.duration());
+
+fill(50, 200, 200);
+ellipse(musicFile.currentTime()*20,480-amp.getLevel()*1000, 20, 20);
+
 }
+
+
+
+
+
+
+// let img, song;
+// let vol = 0.5;
+
+// function preload(){
+//   song = loadSound('cpr.mp3');
+// }
+
+// function setup(){
+//   createCanvas(img.width, img.height);
+//   playBtn = createButton('PLAY');
+//   playBtn.mousePressed(mp3_play);
+//   volume = createSlider(0, 1, vol, 0.01);
+//   volume.size(300);
+// }
+// function mp3_play(){
+//   if(song.isPlaying()){
+//     song.pause();
+//     playBtn.html('PLAY');
+//     background(255);
+//   }else{
+//     song.play();
+//     playBtn.html('PAUSE');
+//     background(img);
+//   }
+// }
+
+// function draw(){
+//   song.setVolume(volume.value());
+// } -볼륨조절
